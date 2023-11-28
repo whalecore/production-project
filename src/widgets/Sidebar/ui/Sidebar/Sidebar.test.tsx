12d90c509cdf87b788b2775d078d2b414d6d755f
r.gaskarov <komnatakita@gmail.com>
2023-11-28 22:08:33 +0300

    update actions config, add test renderer for routes

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,20 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react'
-import { withTranslation } from 'react-i18next'
-import { renderWithTranslations } from 'shared/lib/tests/renderWithTranslations/renderWithTranslations'
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender'
 import { Sidebar } from 'widgets/Sidebar'
 
 describe('Sidebar', () => {
   test('render Sidebar', () => {
-    const SidebarWithTranslation = withTranslation()(Sidebar)
-
-    renderWithTranslations(<SidebarWithTranslation />)
+    componentRender(<Sidebar />)
     expect(screen.getByTestId('Sidebar')).toBeInTheDocument()
   })
 
   test('test toggle', () => {
-    const SidebarWithTranslation = withTranslation()(Sidebar)
-
-    renderWithTranslations(<SidebarWithTranslation />)
+    componentRender(<Sidebar />)
     const toggleBtn = screen.getByTestId('sidebar-toggle')
     fireEvent.click(toggleBtn)
     expect(screen.getByTestId('Sidebar')).toHaveClass('collapsed')
